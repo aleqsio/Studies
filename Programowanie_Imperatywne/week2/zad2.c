4cc45c04a4742886c241f442b9f375b862b61312
Aleksander Mikucki <mikucki@gmail.com>
2017-05-07 22:06:00 +0200

    still working on pi homework created new logic homework project

--- Programowanie_Imperatywne/week2/zad2.c
+++ Programowanie_Imperatywne/week2/zad2.c
@@ -1,66 +1,81 @@
-#define _XOPEN_SOURCE
-#define MIN	0
-#define MAX 1
-#define MINIMUM(x,y) x<y ? x : y
+#define _XOPEN_SOURCE 700
+#define MIN(x,y) (x<y ? x : y)
+#define MAX(x,y) (x>y ? x : y)
 #include <stdio.h>
+#include <stdlib.h>
+
 struct size {
-	int x;
-	int y;
+    int x;
+    int y;
 };
 void fill(int** t, struct size s)
 {
-	for (int i = 0; i < s.y; i++)
-	{
-		for (int j = 0; j < s.x; j++)
-		{
-			t[i][j] = MIN + rand() % (MAX - MIN + 1);
-		}
-	}
+    for (int i = 0; i < s.y; i++)
+    {
+        for (int j = 0; j < s.x; j++)
+        {
+            t[i][j] = 0;
+        }
+    }
+}
+void read(int** t,struct size s)
+{
+    for (int i = 0; i < s.y; i++)
+    {
+        for (int j = 0; j < s.x; j++)
+        {
+            scanf("%d", &t[i][j]);
+        }
+    }
 }
 void print(int** t, struct size s)
 {
-	for (int i = 0; i < s.y; i++)
-	{
-		for (int j = 0; j < s.x; j++)
-		{
-			printf("%d,", t[i][j]);
-		}
-		printf("\n");
-	}
+    for (int i = 0; i < s.y; i++)
+    {
+        for (int j = 0; j < s.x; j++)
+        {
+            printf("%d ", t[i][j]);
+        }
+        printf("\n");
+    }
 }
 int** create_two_dim_array(struct size s)
 {
-	int** a = malloc(sizeof(int*)*s.y);
-	for (int i = 0; i < s.x; i++)
-	{
-		a[i] = malloc(sizeof(int)*s.x);
-	}
-	return a;
+    int** a = malloc(sizeof(int*)*s.y);
+    for (int i = 0; i < s.x; i++)
+    {
+        a[i] = malloc(sizeof(int)*s.x);
+    }
+    return a;
+}
+void populate(int** res,int** t,struct size s,int r)
+{
+    for (int i = 0; i < s.y; i++)
+    {
+        for (int j = 0; j < s.x; j++)
+        {
+            for (int k = MAX(0,i-r); k <= MIN(i+r,s.y-1); k++)
+            {
+                for (int l = MAX(0,j-r); l <= MIN(j+r,s.x-1); l++)
+                {
+                  res[k][l]+=t[i][j];
+                }
+            }
+        }
+    }
 }
 int main(void)
 {
-	struct size s1;
-	s1.x = SZ;
-	s1.y = SZ;
-	struct size s2;
-	s2.x = SZ;
-	s2.y = SZ;
-	int**a = create_two_dim_array(s1);
-	int**b = create_two_dim_array(s2);
-	fill(a, s1);
-	fill(b, s2);
-	print(a, s1);
-	print(b, s2);
-	printf("%d ", sum_diagonal(a, s1));
-	printf("%d ", sum_under_diagonal(a, s1));
-	struct size s3;
-	s3.x = s2.x;
-	s3.y = s1.y;
-	int**c = create_two_dim_array(s3); //matrix multipication requires square array
-	printf("\n\n\n");
-	multiplyarray(a, b, c, s1, s2, s3);
-	print(c, s3);
-
-	return 0;
+    struct size s;
+    int r;
+    scanf("%d %d",&s.x,&r);
+    s.y=s.x;
+    int** arr = create_two_dim_array(s);
+    int** res = create_two_dim_array(s);
+    read(arr,s);
+    fill(res,s);
+    populate(res,arr,s,r);
+    print(res,s);
+    return 0;
 }
 
