3a3aa707fb382a7d1cbe24fcfcb6c5ca2f4368ad
Aleksander Mikucki <mikucki@gmail.com>
2017-05-10 15:13:41 +0200

    v3

--- Programowanie_Imperatywne/kartkowka2/main.c
+++ Programowanie_Imperatywne/kartkowka2/main.c
@@ -7,11 +7,8 @@ int max(int* arr,int i,int end)
     arr[i] = i%2 ? arr[i/2]+arr[(i+1)/2] :  arr[i/2];
     if(i==end)
         return arr[i];
-    else
-    {
         int next=max(arr,i+1,end);
         return next>arr[i] ? next : arr[i];
-    }
 }
 int main() {
     int n;
